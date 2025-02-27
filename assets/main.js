11bb6c99df5fcc1ee215523d4c8b7f56091d01f8
alexciccarese <alex.ciccarese@gmail.com>
2025-02-27 15:12:31 +0100

    add mail with fetch

--- assets/main.js
+++ assets/main.js
@@ -29,5 +29,48 @@ axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
 })
 
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(function (response) {
+  const randomEmail = response.data.response
+
+  console.log(randomEmail);
+  
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+})
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(function (response) {
+  const randomEmail = response.data.response
+
+  console.log(randomEmail);
+  
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+})
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
 
 
