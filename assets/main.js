e9bba47d374e2050de59453286483e9aeaeae18d
alexciccarese <alex.ciccarese@gmail.com>
2025-02-27 13:55:01 +0100

    add api with axios

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,9 @@
 const emailListEl = document.querySelector('.email_list')
 
+
+//add API with axios
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(function (response) {
+  console.log(response.data);
+  
+})
