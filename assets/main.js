706aae113a94f9241726ba6a5ad8361354db1001
alexciccarese <alex.ciccarese@gmail.com>
2025-02-27 14:58:46 +0100

    add random email

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,18 @@
 const emailListEl = document.querySelector('.email_list')
 
-
 //add API with axios
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 .then(function (response) {
-  console.log(response.data);
+  const randomEmail = response.data.response
+
+  console.log(randomEmail);
   
+  
+  const newList = document.createElement('li')
+  newList.textContent = randomEmail
+  
+  emailListEl.appendChild(newList)
 })
+
+
+
