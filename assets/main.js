16012b4dcf451b634d8e1095135b6aacda2b2a73
alexciccarese <alex.ciccarese@gmail.com>
2025-02-27 15:13:43 +0100

    add all email

--- assets/main.js
+++ assets/main.js
@@ -73,4 +73,83 @@ fetch('https://flynn.boolean.careers/exercises/api/random/mail')
 })
 
 
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => response.json())
+.then(data => {
+  console.log(data.response);
+
+  const randomEmail = data.response
+  
+  const emailOne = document.createElement('li')
+  emailOne.textContent = randomEmail
+  
+  emailListEl.appendChild(emailOne)
+
+
+})
+
+
 
